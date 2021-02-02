33e1520353ebbe4b81e8262a041c67d04f2517f8
Sam Hall <mann.samantha11@gmail.com>
2021-02-01 22:02:51 -0500

    commit tree 3722190518dff3dcc516935b4c7befe7ac2bdab0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+// // Code your solutions in this file
+
+
+
+function writeCards(array, event) {
+  const cards = []
+  for (let i = 0; i < array.length; i++) {
+    cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards
+}
+
+function countDown(num) {
+  let countdown = num;
+  while (countdown >= 0) {
+  console.log(countdown--);
+}
+}
+countDown(10)
